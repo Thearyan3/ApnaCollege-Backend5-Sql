f2da40e7ccd5138961da4ef489bb82d29cb1f5e5
Aryan <chaudharyaryan3456@gmail.com>
2025-02-11 16:21:39 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app2',
+    password: 'Sigmaaryan2024'
+});
+
+try{
+connection.query("SHOW TABLES", (err, result) => {
+    if(err) throw err;
+    console.log(result);
+});
+}catch(err){
+    console.log(err);
+}
+
+connection.end();
 
 let getRandomUser = () => {
     return {
-      Id: faker.string.uuid(),
-      username: faker.internet.username(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
+        Id: faker.string.uuid(),
+        username: faker.internet.username(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
     };
-  }
-
-  console.log(getRandomUser());
+}
